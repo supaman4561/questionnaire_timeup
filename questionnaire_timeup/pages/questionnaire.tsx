a3df4c31c9da25c1052247c844bdf68f9fd9cd0d
dosu <dosu416@gmail.com>
2022-06-09 00:32:54 +0900

    feat: display question

--- questionnaire_timeup/pages/questionnaire.tsx
+++ questionnaire_timeup/pages/questionnaire.tsx
@@ -1,9 +1,34 @@
+import { GetStaticProps } from 'next'
 import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
 
-const Questionnaire: NextPage = () => {
+type Questions = {
+  questions: Question[]
+}
+
+type Question = {
+  id: number
+  jp: string
+  question: string
+}
+
+const Questionnaire: NextPage<Questions> = ({ questions } : Questions) => {
+
+  let count = 0
+  console.log(questions.questions[0])
+
   return (
-    <h1> Questionnaire Page </h1>
+    <main className={styles.main}>
+      <p> {questions.questions[count].jp} </p>
+      <p> {questions.questions[count].question} </p>
+    </main>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch('http://localhost:3000/api/questionnaire/pre')
+  const questions = await res.json()
+  return { props: { questions } }
 }
 
 export default Questionnaire
