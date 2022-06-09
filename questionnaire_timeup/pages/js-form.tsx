02352968e42420a59182b2745773487149cc5dad
Takahito Sudo <dosu416@gmail.com>
2022-06-09 19:10:44 +0900

    feat: add timer

--- questionnaire_timeup/pages/js-form.tsx
+++ questionnaire_timeup/pages/js-form.tsx
@@ -1,19 +1,38 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
+import style from "../styles/Home.module.css"
+import Timer from "./timer"
 
-const PageWithJsbasedForm = ({ id, onSubmitForm}: any) => {
+const PageWithJsbasedForm = ({ id, onSubmitForm }: any) => {
 
+  const maxSec = 20
   const [answer, setAnswer] = useState("")
-  // Handles the submit event on form submit.
-  const handleSubmit = async (event: any) => {
-    // Stop the form from sbmitting and refreshing the page.
-    event.preventDefault()
+  const [sec, setSec] = useState(maxSec)
+  const btn = useRef<HTMLButtonElement>(null)
 
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      if (sec-1 > 0) {
+        setSec(sec-1)
+      } else {
+        btn.current?.click()
+        setSec(maxSec)
+      }
+      console.log(sec)
+    }, 1000)
+    return () => {
+      clearInterval(id)
+    }
+  }, [sec])
+
+
+  const submit = async () => {
     const time: number = onSubmitForm()
 
     // Get data from the form.
     const data = {
       id: id,
-      answer: event.target.answer.value,
+      answer: answer,
       time: time
     }
 
@@ -38,18 +57,34 @@ const PageWithJsbasedForm = ({ id, onSubmitForm}: any) => {
     const response = await fetch(endpoint, options)
 
     setAnswer('')
+    setSec(maxSec)
     
     const result = await response.json()
     console.log(result)
+
+  }
+
+  // Handles the submit event on form submit.
+  const handleSubmit = async (event: any) => {
+    // Stop the form from sbmitting and refreshing the page.
+    event.preventDefault()
+    submit()
   }
   
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="answer">Answer: </label>
-      <input type="text" id="answer" name="answer" value={answer} 
-        onChange={(e)=>{setAnswer(e.target.value)}}/>
-      <button type="submit">回答</button>
-    </form>
+    <div className={style.grid}>
+      <div className={style.grid}>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="answer">Answer: </label>
+          <input type="text" id="answer" name="answer" value={answer} 
+            onChange={(e)=>{setAnswer(e.target.value)}}/>
+          <button ref={btn} type="submit">回答</button>
+        </form>
+      </div>
+      <div className={style.grid}>
+        <Timer sec={sec} maxSec={maxSec}/>
+      </div>
+    </div>
   )
 }
 
