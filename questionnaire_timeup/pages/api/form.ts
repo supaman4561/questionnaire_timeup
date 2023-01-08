0b0c127ce0e189266c5227f00e6696ee6334047d
Takahito Sudo <dosu416@gmail.com>
2023-01-08 18:42:03 +0900

    modify: ログ出力を標準出力からログファイル書き出しに変更

--- questionnaire_timeup/pages/api/form.ts
+++ questionnaire_timeup/pages/api/form.ts
@@ -1,4 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { format } from 'date-fns'
+import * as fs from 'fs'
+import { EOL } from 'os'
 
 export default function handler(
   req: NextApiRequest,
@@ -6,9 +9,30 @@ export default function handler(
 ) {
   if (req.method === 'POST') {
     const body = req.body
-    console.log(
-      `UserID:${body.userid},SetID:${body.setid},QID:${body.qid},Answer:${body.answer},Time:${body.time/1000}`
-    )
+    const logFilePath = `log/${body.userid}.csv`
+    const header = "DateTime,UserID,Section,QuestionID,Answer,Time"
+    const logData = [
+      format(new Date().getTime(), 'yyyy/MM/dd HH:mm:ss'),
+      body.userid,
+      body.setid,
+      body.qid,
+      body.answer,
+      body.time/1000
+    ].join(',')
+
+    try {
+      if (!fs.existsSync(logFilePath)) {
+        // file does not exist
+        fs.writeFileSync(logFilePath, header + EOL)  // write csv header
+      }
+      fs.appendFile(logFilePath, logData + EOL, 'utf-8', (err) => {
+        if (err) throw err;
+      })
+    } catch(err) {
+      console.error(err);
+    }
+    
+    console.log(logData);
     res.status(200).json({ data: `${body.answer}`})
   }
 }
